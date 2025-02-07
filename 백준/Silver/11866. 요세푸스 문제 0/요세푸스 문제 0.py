cfc81b99d8075e562d81c888d7b550cc3b0bdc91
2Moogi <78463220+2Moogi@users.noreply.github.com>
2025-02-08 01:19:03 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 108 ms, Memory: 34924 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
@@ -0,0 +1,25 @@
+from collections import deque
+import sys
+input=sys.stdin.readline
+
+n, k = map(int,input().split())
+
+Q = deque()
+cnt = 0
+for i in range(1,n+1):
+    Q.append(i)
+arr=[]
+while Q:
+    cnt+=1
+    if cnt == k:
+        arr.append(Q.popleft())
+        cnt = 0
+    else:
+        Q.append(Q.popleft())
+print('<', end='')
+for i in range(len(arr)):
+    if i == len(arr)-1:
+        print(arr[i], end='')
+    else:
+        print(arr[i], end=', ')
+print('>')
